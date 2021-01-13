78349cf16634a835ef044a9b3402834a9204331f
Tree <325894097@qq.com>
2021-01-13 22:28:24 +0800

    第一周作业

---
+++ Week_01/HelloClassLoader.java
@@ -0,0 +1,64 @@
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @ClassName HelloClassLoader
+ * @description
+ */
+public class HelloClassLoader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File file = new File("G:\\test\\Hello.xlass");
+        try {
+            byte[] bytes = File2byte(file);
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return super.findClass(name);
+    }
+
+    /**
+     * 将文件转换成byte数组
+     *
+     * @param filePath
+     * @return
+     */
+    public static byte[] File2byte(File filePath) {
+        byte[] buffer = null;
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] b = new byte[1024];
+            int n;
+            while ((n = fis.read(b)) != -1) {
+                bos.write(b, 0, n);
+            }
+            fis.close();
+            bos.close();
+            buffer = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = new HelloClassLoader().findClass("Hello");
+            Object obj = aClass.newInstance();
+            Method method = aClass.getMethod("hello");
+            method.invoke(obj);
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
